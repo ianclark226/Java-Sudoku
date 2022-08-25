da96cb4f87879a93eb6f72aae897c68bba9647c1
Ian Clark <ianclark226@gmail.com>
2022-08-25 11:13:44 -0400

    calculator completed

---
+++ sudoku/src/main/java/ianclark226/IStorage.java
@@ -0,0 +1,8 @@
+// package ianclark226;
+
+// import java.io.IOException;
+
+// public interface IStorage {
+//     void updateGameData(SudokuGame gmae) throws IOException;
+//     SudokuGame getGameData() throws IOException;
+// }
---
+++ sudoku/src/main/java/ianclark226/SudokuUtilities.java
@@ -0,0 +1,5 @@
+package ianclark226;
+
+public class SudokuUtilities {
+
+}
